a854e121a2e9cc3ea924df15267544ad3126a6d8
AlbertoDamaso <alberto.matheus@hotmail.com>
2021-03-26 12:57:33 -0300

    comentarios

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {
     View,
     Text, 
-    SytleSheet,
+    StyleSheet,
     TextInput,
     TouchableWithoutFeedback as TWF,
     Alert
@@ -20,7 +20,57 @@ class AddComment extends Component {
     }
 
     render() {
+        let commentArea = null
+        if (this.state.editMode) {
+            commentArea = (
+                <View style={styles.container}>
+                    <TextInput placeholder='Pode comentar...'
+                        style={styles.input} autoFocus={true}
+                        value={this.state.comment}
+                        onChangeText={comment => this.setState({ comment})}
+                        onSubmitEditing={this.handleAddComment}/>
+                    <TWF onPress={() => this.setState ({editMode: false})}>    
+                        <Icon name='times' size={15} color='#555'/>
+                    </TWF>                        
+                </View>
+            )
+        } else {
+            commentArea = (
+                <TWF onPress={() => this.setState({ editMode: true})}>
+                    <View style={styles.container}>
+                        <Icon name='comment-o' size={25} color='#555'/>
+                        <Text style={styles.caption}>
+                            Adicione um comentario...
+                        </Text>
+                    </View>                        
+                </TWF>
+            )
+        }
+        return (
+            <View style={{ flex: 1 }}>
+                {commentArea}
+            </View>
+        )
         
     }
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10
+    },
+    caption: {
+        marginLeft: 10,
+        fontSize: 12,
+        color: '#CCC'
+    },
+    input: {
+        width: '90%'
+    }
+})
+
+export default AddComment
