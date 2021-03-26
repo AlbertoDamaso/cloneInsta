5471a9038b70394427d432b9503ddbefce62f988
AlbertoDamaso <alberto.matheus@hotmail.com>
2021-03-26 10:27:40 -0300

    cloneInsta

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import Header from './src/components/Header'
+import { View } from 'react-native'
+import Post from './src/components/Post'
+
+class App extends Component {
+  render() {
+    const comments = [{
+      nickname: 'Jose Pinto',
+      comment: 'Excelente Foto!'
+    }, {
+      nickname: 'Ana Carolina',
+      comment: 'Muito ruim @Betim, faça melhor...'
+    }]
+
+    return(
+      <View style={{flex:1}}>
+        <Header/>
+        <Post image={require('./assets/imgs/fence.jpg')}
+          comments={comments}/>
+      </View>
+    )
+  }
+}
+
+export default App
---
+++ src/components/AddComment.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {
+    View,
+    Text, 
+    SytleSheet,
+    TextInput,
+    TouchableWithoutFeedback as TWF,
+    Alert
+} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+class AddComment extends Component {
+    
+}
